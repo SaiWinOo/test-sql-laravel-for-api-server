14a517363affc3fc643d5d5ac8a0f133fb6bd897
SaiWinOo <saiwinoo52@gmail.com>
2023-09-10 10:50:53 +0630

    done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
+});
+
+Route::get('users', function(){
+    return User::all();
 });
