abadf72215752bf37a5f64c1ef02f7cd044d078e
SaiWinOo <saiwinoo52@gmail.com>
2023-09-10 12:40:01 +0630

    done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
+    Artisan::call('optimize:clear');
     return view('welcome');
 });
 
